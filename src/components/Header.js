124e8d181cd360e84822383ba751c998e586365f
cicaksafija <162234691+cicaksafija@users.noreply.github.com>
2024-05-27 12:21:10 +0200

    kontakt ispravka

--- src/components/Header.js
+++ src/components/Header.js
@@ -102,7 +102,7 @@ export const Header = () => {
                                     nama</a>
                             </li>
                             <li>
-                                <a href="#"
+                                <a href="/Kontakt"
                                    className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Kontakt</a>
                             </li>
                             <li>
